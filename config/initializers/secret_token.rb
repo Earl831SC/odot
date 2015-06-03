f9641608c8e9fbb30fa93797fa2d3b8696087d91
Earl831SC <ryanjudd31@gmail.com>
2015-06-03 12:33:19 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Odot::Application.config.secret_token = '884d5ec185cd8406c4f290d7b64589c8c3545715e29d63155f08183e665009a3a270d09fd2a665d73dbf8a1436cc63bfe1b695c3e8e93a2a93342e8d5451f7f2'
